babfb9d80b66b2658044effd5bebc9552bdd1f6b
Ngo Quang Huy <huynqgch17251@fpt.edu.vn>
2020-11-16 01:48:29 +0700

    update

--- www/js/database.js
+++ www/js/database.js
@@ -1,4 +1,4 @@
-const listRes = [
+const RestaurantData = [
     {
         res_name: 'Restaurant demo',
         res_type: 'Demo',
@@ -41,44 +41,24 @@ const listRes = [
     }
 ]
 
-var db;
-var request = window.indexedDB.open("Restaurant-Database", 2);
+var data;
+var requestDB = window.indexedDB.open("RestaurantData", 1);
 request.onupgradeneeded = function(event) {
-    var db = event.target.result;
-    var objectStore = db.createObjectStore("RestaurantDatabase", {keyPath: "id", autoIncrement: true});
-    for (var i in listRes) {
-        objectStore.add(listRes[i])
+    var data = event.target.result;
+    var objectStore = data.createObjectStore("RestaurantData", {keyPath: "id", autoIncrement: true});
+    for (var i in RestaurantData) {
+        objectStore.add(RestaurantData[i])
     }
 }
 
 request.onsuccess = function(event) {
-    db = request.result;
-    console.log("Success: " + db);
+    data = request.result;
+    console.log("Successfull!: " + data);
 }
 
-function getAllData(collectionName) {
-    const transaction = db.transaction([collectionName], "readonly");
+function LoadDataFrDB(collectionName) {
+    const transaction = data.transaction([collectionName], "readonly");
     const objectStore = transaction.objectStore(collectionName);
     request = objectStore.getAll();
     return request;
 }
-$(window).on('load', function(){
-    let result = getAllData("RestaurantDatabase")
-    result.onsuccess = function(event){
-        let data = event.target.result
-        console.log(data)
-        for(let i in data){
-            let newcontent = `<div class="col-md-4 text-center col-sm-6 col-xs-6">
-        <div class="thumbnail product-box">
-            <div class="caption">
-                <h3><a href="#">${data[i].res_name}</a></h3>
-                <p><a href="feedback.html" class="btn btn-success" role="button">Feedback</a> <a href="#" class="btn btn-primary" role="button">See Details</a></p>
-            </div>
-        </div>
-        </div>`
-
-        $('.app').append(newcontent)
-        }
-        
-    }
-})
